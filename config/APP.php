9e2a9a4d90a54fddd15afede85a38a7543b52232
w1g9t9t8 <44007513+w1g9t9t8@users.noreply.github.com>
2021-11-09 21:05:42 -0500

    Add files via upload

---
+++ config/APP.php
@@ -0,0 +1,8 @@
+<?php
+	const SERVERURL="http://localhost/Vikao/";
+
+	const COMPANY="VIKAO";
+
+	const MONEDA="S/.";
+
+	date_default_timezone_set("America/Lima");
---
+++ config/SERVER.php
@@ -0,0 +1,13 @@
+<?php
+
+	const SERVER="localhost";
+	const DB="waldos";
+	const USER="root";
+	const PASS="";
+
+	const SGBD="mysql:host=".SERVER.";dbname=".DB;
+
+
+	const METHOD="AES-256-CBC";
+	const SECRET_KEY='$WALDOS@2021';
+	const SECRET_IV='037970';
